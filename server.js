f9ec4f746457cb1332f545f2b99b4593a49643b4
Anand Kumar Singh <aks858271@gmail.com>
2023-01-28 19:51:26 +0530

    Vehicle adding route created

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.post('/vehicles/add',()=>{
+  res.send("Data of Vehicles is saved!");
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
