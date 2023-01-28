13589a87a343bbc0f04716a6a7ecf6eed81153a1
ayushtiw <kamleshtiwary753@gmail.com>
2023-01-28 19:51:08 +0530

    created model

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const mongoose = require("mongoose");
+const app = express()
+const port = 3000
+mongoose.set('strictQuery',false);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+const mongooose =require('mongoose');
+const laundeSchema =mongooose.Schema({
+    Name:String,
+    Description:String,
+    age:Number
+    
+});
+
+const laundeModel=mongooose.model('/',laundeSchema);
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
